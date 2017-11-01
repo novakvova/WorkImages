c4db58336b6b4f3eaf71275aedefc8abe3d1ca0d
novak vova <novakvova@gmail.com>
2017-11-01 18:33:06 +0200

    WorkImage Good

---
+++ WebSiteDefault/Controllers/GalleryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteDefault.Helpers;
+
+namespace WebSiteDefault.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(HttpPostedFileBase image)
+        {
+            Bitmap imageSave = WorkImage.CreateImage(image, 800, 600);
+            if (imageSave != null)
+            {
+                string path = Server.MapPath(ConfigurationManager.AppSettings["ImagePathGallery"]);
+                string fileName = Guid.NewGuid().ToString() + ".jpg";
+                imageSave.Save(path+fileName, ImageFormat.Jpeg);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebSiteDefault/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.DataProtection;
+using Owin;
+using System.Web;
+using System.Web.Mvc;
+using WebSiteDefault.Models;
+
+[assembly: OwinStartupAttribute(typeof(WebSiteDefault.Startup))]
+namespace WebSiteDefault
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var builder = new ContainerBuilder();
+
+            // REGISTER DEPENDENCIES
+            builder.RegisterType<ApplicationDbContext>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationUserStore>().As<IUserStore<ApplicationUser>>().InstancePerRequest();
+            builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            builder.Register<IAuthenticationManager>(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+            builder.Register<IDataProtectionProvider>(c => app.GetDataProtectionProvider()).InstancePerRequest();
+
+            // REGISTER CONTROLLERS SO DEPENDENCIES ARE CONSTRUCTOR INJECTED
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // BUILD THE CONTAINER
+            var container = builder.Build();
+
+            // REPLACE THE MVC DEPENDENCY RESOLVER WITH AUTOFAC
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // REGISTER WITH OWIN
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacMvc();
+
+            ConfigureAuth(app);
+        }
+    }
+}
