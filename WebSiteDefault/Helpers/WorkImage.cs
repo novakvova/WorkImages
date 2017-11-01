309c90e5e1ee8b3592e1b93b69e8835d99ed63fe
novak vova <novakvova@gmail.com>
2017-11-01 18:44:49 +0200

    Update resize image

--- WebSiteDefault/Helpers/WorkImage.cs
+++ WebSiteDefault/Helpers/WorkImage.cs
@@ -17,32 +17,43 @@ namespace WebSiteDefault.Helpers
                 {
                     using (Bitmap originalPic = new Bitmap(hpf.InputStream, true))
                     {
-                        int width = originalPic.Width;
-                        int height = originalPic.Height;
-                        int widthDiff = width - maxWidth;
-                        int heightDiff = height - maxHeight;
-                        bool doWidthResize = (maxWidth > 0 && width > maxWidth && widthDiff > heightDiff);
-                        bool doHeightResize = (maxHeight > 0 && height > maxHeight && heightDiff > widthDiff);
 
-                        if(doWidthResize || doHeightResize || (width.Equals(height) && widthDiff.Equals(heightDiff)))
-                        {
-                            int iStart;
-                            Decimal divider;
-                            if(doWidthResize)
-                            {
-                                iStart = width;
-                                divider = Math.Abs((Decimal)iStart / maxWidth);
-                                width = maxWidth;
-                                height = (int)Math.Round((height / divider));
-                            }
-                            else
-                            {
-                                iStart = height;
-                                divider = Math.Abs((Decimal)iStart / maxHeight);
-                                height = maxHeight;
-                                width = (int)Math.Round(width / divider);
-                            }
-                        }
+                        int originalWidth = originalPic.Width;
+                        int originalHeight = originalPic.Height;
+                        // To preserve the aspect ratio
+                        float ratioX = (float)maxWidth / (float)originalWidth;
+                        float ratioY = (float)maxHeight / (float)originalHeight;
+                        float ratio = Math.Min(ratioX, ratioY);
+                        // New width and height based on aspect ratio
+                        int width = (int)(originalWidth * ratio);
+                        int height = (int)(originalHeight * ratio);
+                        //int width = originalPic.Width;
+                        //int height = originalPic.Height;
+                        //int widthDiff = width - maxWidth;
+                        //int heightDiff = height - maxHeight;
+                        //bool doWidthResize = (maxWidth > 0 && width > maxWidth && widthDiff > heightDiff);
+                        //bool doHeightResize = (maxHeight > 0 && height > maxHeight && heightDiff > widthDiff);
+
+                        //if(doWidthResize || doHeightResize || (width.Equals(height) && widthDiff.Equals(heightDiff)))
+                        //{
+                        //    int iStart;
+                        //    Decimal divider;
+                        //    if(doWidthResize)
+                        //    {
+                        //        iStart = width;
+                        //        divider = Math.Abs((Decimal)iStart / maxWidth);
+                        //        width = maxWidth;
+                        //        height = (int)Math.Round((height / divider));
+                        //    }
+                        //    else
+                        //    {
+                        //        iStart = height;
+                        //        divider = Math.Abs((Decimal)iStart / maxHeight);
+                        //        height = maxHeight;
+                        //        width = (int)Math.Round(width / divider);
+                        //    }
+                        //}
+
                         using (Bitmap outBmp = new Bitmap(width, height, PixelFormat.Format24bppRgb))
                         {
                             using (Graphics oGraphics = Graphics.FromImage(outBmp))
